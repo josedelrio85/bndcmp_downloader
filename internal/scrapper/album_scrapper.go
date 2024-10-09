78a0a56a2ce3935c6f1aa7ca6a12d61e30a6fd64
Jose del Río <jose.del.rio@deus.ai>
2024-10-09 13:07:54 +0200

    album scrapper implementation

---
+++ internal/scrapper/album_scrapper.go
@@ -0,0 +1,71 @@
+package scrapper
+
+import (
+	io "io"
+	"regexp"
+	"strings"
+
+	html "golang.org/x/net/html"
+)
+
+type AlbumScrapper struct {
+	TrackList []string
+}
+
+func NewAlbumScrapper() *AlbumScrapper {
+	return &AlbumScrapper{
+		TrackList: []string{},
+	}
+}
+
+func (a *AlbumScrapper) Retrieve(url string) (io.Reader, error) {
+	return retrieve(url)
+}
+
+func (a *AlbumScrapper) Parse(data io.Reader) (*html.Node, error) {
+	return parse(data)
+}
+
+func (a *AlbumScrapper) Find(node *html.Node) error {
+	if err := a.find(node); err != nil {
+		return err
+	}
+	a.TrackList = a.processTrackList()
+	return nil
+}
+
+func (a *AlbumScrapper) find(node *html.Node) error {
+	if node.Type == html.ElementNode && node.Data == "a" {
+		for _, attr := range node.Attr {
+			if attr.Key == "href" && strings.Contains(attr.Val, "track") {
+				a.TrackList = append(a.TrackList, attr.Val)
+			}
+		}
+	}
+
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		if err := a.find(c); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (a *AlbumScrapper) processTrackList() []string {
+	pattern := regexp.MustCompile(`^/track/[a-z0-9-]+$`)
+	seen := make(map[string]bool)
+	var result []string
+
+	for _, track := range a.TrackList {
+		if pattern.MatchString(track) && !seen[track] {
+			result = append(result, track)
+			seen[track] = true
+		}
+	}
+
+	return result
+}
+
+func (a *AlbumScrapper) Save(data io.Reader, filename string, folder *string) error {
+	return save(data, filename, folder)
+}
