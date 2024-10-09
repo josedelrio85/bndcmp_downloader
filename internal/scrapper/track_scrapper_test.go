c57dc178a92a71b24d70d918b5c022b44bd4f9e2
Jose del Río <jose.del.rio@deus.ai>
2024-10-09 16:43:14 +0200

    created unit test for track scrapper Find method

--- internal/scrapper/track_scrapper_test.go
+++ internal/scrapper/track_scrapper_test.go
@@ -2,13 +2,21 @@ package scrapper
 
 import (
 	"bytes"
+	_ "embed"
 	"errors"
 	"testing"
 
 	gomock "github.com/golang/mock/gomock"
+	"github.com/josedelrio85/bndcmp_downloader/internal/model"
 	"github.com/stretchr/testify/suite"
 	html "golang.org/x/net/html"
 )
+
+//go:embed node_example.xml
+var validExample string
+
+//go:embed invalid_node_example.xml
+var invalidExample string
 
 func TestTrackScrapper(t *testing.T) {
 	suite.Run(t, new(TestTrackScrapperSuite))
@@ -41,8 +49,8 @@ func (s *TestTrackScrapperSuite) TestRetrieve_Success() {
 
 	reader, err := s.scrapper.Retrieve(s.trackURL)
 
-	s.Require().NoError(err)
-	s.Require().NotNil(reader)
+	s.NoError(err)
+	s.NotNil(reader)
 }
 
 func (s *TestTrackScrapperSuite) TestRetrieve_Error() {
@@ -51,9 +59,9 @@ func (s *TestTrackScrapperSuite) TestRetrieve_Error() {
 
 	reader, err := s.scrapper.Retrieve(s.trackURL)
 
-	s.Require().Error(err)
-	s.Require().Equal(expectedError, err)
-	s.Require().Nil(reader)
+	s.Error(err)
+	s.Equal(expectedError, err)
+	s.Nil(reader)
 }
 
 func (s *TestTrackScrapperSuite) TestParse_Success() {
@@ -79,4 +87,31 @@ func (s *TestTrackScrapperSuite) TestParse_Error() {
 	s.Error(err)
 	s.Equal(expectedError, err)
 	s.Nil(node)
+}
+
+func (s *TestTrackScrapperSuite) TestFind_Success() {
+	dataAsBytes := []byte(validExample)
+	mockReader := bytes.NewReader(dataAsBytes)
+	nodes, err := html.Parse(mockReader)
+	s.NoError(err)
+	s.NotNil(nodes)
+
+	err = s.scrapper.Find(nodes)
+	s.NoError(err)
+	s.Equal("Elbow", s.scrapper.Track.Title)
+	s.Equal("https://kinggizzard.bandcamp.com/track/elbow", s.scrapper.Track.URL)
+	s.Equal("https://t4.bcbits.com/stream/b77ce644d30f5a71778080be8c194c19/mp3-128/3749823254?p=0&ts=1728551843&t=dd8cc7cd9d747ac5be9c0a202fea450a5aa08944&token=1728551843_656b69850113f6ea23cd1e4321e6d148a256413b", s.scrapper.Track.DownloadURL)
+}
+
+func (s *TestTrackScrapperSuite) TestFind_Error() {
+	dataAsBytes := []byte(invalidExample)
+	mockReader := bytes.NewReader(dataAsBytes)
+	nodes, err := html.Parse(mockReader)
+	s.NoError(err)
+	s.NotNil(nodes)
+
+	err = s.scrapper.Find(nodes)
+	s.Error(err)
+	s.Equal(&model.Track{}, s.scrapper.Track)
+	s.Contains(err.Error(), "invalid character")
 }
