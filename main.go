daae5529f4e99674cb50e541826d28f901533f92
Jose del Río <jose.del.rio@deus.ai>
2024-10-11 14:10:11 +0200

    modified main function to set up all dependencies and the prompt steps and fire the correct scrapper

--- main.go
+++ main.go
@@ -1,52 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"net/url"
+	"log"
 
 	"github.com/josedelrio85/bndcmp_downloader/internal/parser"
+	"github.com/josedelrio85/bndcmp_downloader/internal/prompt"
 	"github.com/josedelrio85/bndcmp_downloader/internal/retriever"
 	"github.com/josedelrio85/bndcmp_downloader/internal/saver"
 	"github.com/josedelrio85/bndcmp_downloader/internal/scrapper"
 )
 
 func main() {
-	fmt.Println("starting Bandcamp downloader")
+	log.Println("Starting Bandcamp downloader")
 
+	promptChain := setupPromptChain()
+
+	httpClient, parseClient, saveClient := setup(&promptChain.ChainMessage.StorageType)
+
+	var err error
+	switch promptChain.ChainMessage.ScrapType {
+	case scrapper.Track:
+		err = scrapper.NewTrackScrapper(promptChain.ChainMessage.URL.Value, httpClient, parseClient, saveClient).Execute()
+	case scrapper.Album:
+		err = scrapper.NewAlbumScrapper(promptChain.ChainMessage.URL.URL, httpClient, parseClient, saveClient).Execute()
+	case scrapper.Discography:
+		err = scrapper.NewDiscographyScrapper(promptChain.ChainMessage.URL.URL, httpClient, parseClient, saveClient).Execute()
+	default:
+		log.Println("Invalid scrap type")
+	}
+
+	if err != nil {
+		log.Println("Error executing scrapper: ", err)
+	}
+}
+
+func setup(saveFolder *string) (*retriever.HttpClient, *parser.ParseClient, *saver.LocalSaver) {
 	httpClient := retriever.NewHttpClient()
 	parseClient := parser.NewParseClient()
-	saveFolder := "downloads"
-	saveClient := saver.NewLocalSaver(&saveFolder)
+	saveClient := saver.NewLocalSaver(saveFolder)
 
-	// trackURL := "https://kinggizzard.bandcamp.com/track/elbow"
-	// trackScrapper := scrapper.NewTrackScrapper(trackURL, httpClient, parseClient, saveClient)
-	// if err := trackScrapper.Execute(); err != nil {
-	// 	fmt.Println("execute ", err)
-	// 	return
-	// }
+	return httpClient, parseClient, saveClient
+}
 
-	// albumURL := "https://kinggizzard.bandcamp.com/album/12-bar-bruise"
-	// parsedAlbumURL, err := url.Parse(albumURL)
-	// if err != nil {
-	// 	fmt.Println("parsing album url ", err)
-	// 	return
-	// }
-	// albumScrapper := scrapper.NewAlbumScrapper(parsedAlbumURL, httpClient, parseClient, saveClient)
-	// if err := albumScrapper.Execute(); err != nil {
-	// 	fmt.Println("execute ", err)
-	// 	return
-	// }
-
-	discographyURL := "https://kinggizzard.bandcamp.com/music"
-	parsedDiscographyURL, err := url.Parse(discographyURL)
-	if err != nil {
-		fmt.Println("parsing discography url ", err)
-		return
-	}
-	discographyScrapper := scrapper.NewDiscographyScrapper(parsedDiscographyURL, httpClient, parseClient, saveClient)
-	if err := discographyScrapper.Execute(); err != nil {
-		fmt.Println("execute ", err)
-		return
-	}
-
+func setupPromptChain() *prompt.Chain {
+	scrapTypeQuestionLink := prompt.NewScrapTypeQuestionLink()
+	urlCheckerLink := prompt.NewURLCheckerLink()
+	storageQuestionLink := prompt.NewStorageQuestionLink()
+	links := []prompt.Link{scrapTypeQuestionLink, urlCheckerLink, storageQuestionLink}
+	return prompt.NewChain(links)
 }
