d33b0890fc58dfcd54c8d5c8e0fb142e0ca44f31
Jose del Río <jose.del.rio@deus.ai>
2024-10-15 12:19:52 +0200

    created Scrapp method to scrap using the provided url

--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang/mock v1.6.0
 	github.com/gorilla/mux v1.8.1
 	github.com/joho/godotenv v1.5.1
+	github.com/rs/cors v1.11.1
 	github.com/stretchr/testify v1.9.0
 	golang.org/x/net v0.30.0
 	golang.org/x/text v0.19.0
--- backend/internal/handler/handler_test.go
+++ backend/internal/handler/handler_test.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -31,7 +34,6 @@ func (s *HandlerTestSuite) SetupTest() {
 	s.mockDiscographyScrapper = scrapper.NewMockScrapper(s.ctrl)
 	s.mockAlbumScrapper = scrapper.NewMockScrapper(s.ctrl)
 	s.mockTrackScrapper = scrapper.NewMockScrapper(s.ctrl)
-
 	s.handler = NewHttpHandler(
 		baseFolder,
 		s.mockDiscographyScrapper,
@@ -171,4 +173,227 @@ func (s *HandlerTestSuite) TestGetTrack_BadRequest_NoTrack() {
 
 	s.Equal(http.StatusBadRequest, rr.Code)
 	s.Equal("Track is required\n", rr.Body.String())
+}
+
+func (s *HandlerTestSuite) Test_getScrapper() {
+
+	testCases := []struct {
+		desc           string
+		url            string
+		expectedResult bool
+	}{
+		{
+			desc:           "Valid discography URL",
+			url:            "https://testartist.bandcamp.com/music",
+			expectedResult: true,
+		},
+		{
+			desc:           "Valid album URL",
+			url:            "https://testartist.bandcamp.com/album/testalbum",
+			expectedResult: true,
+		},
+		{
+			desc:           "Valid track URL",
+			url:            "https://testartist.bandcamp.com/track/testtrack",
+			expectedResult: true,
+		},
+		{
+			desc:           "Invalid URL",
+			url:            "https://testartist.bandcamp.com/",
+			expectedResult: false,
+		},
+		{
+			desc:           "Invalid URL",
+			url:            "https://testartist.bandcamp.com",
+			expectedResult: false,
+		},
+	}
+
+	for _, tt := range testCases {
+		fmt.Println(tt.desc)
+
+		scrapURL, err := url.Parse(tt.url)
+		s.NoError(err)
+
+		scrapper, err := s.handler.getScrapper(scrapURL)
+
+		if tt.expectedResult {
+			s.NoError(err)
+			s.NotNil(scrapper)
+		} else {
+			s.Error(err)
+			s.Nil(scrapper)
+		}
+	}
+}
+
+func (s *HandlerTestSuite) Test_isValidBandcampURL() {
+	testCases := []struct {
+		desc     string
+		url      string
+		expected bool
+	}{
+		{
+			desc:     "Valid discography URL",
+			url:      "https://testartist.bandcamp.com/music",
+			expected: true,
+		},
+		{
+			desc:     "Valid album URL",
+			url:      "https://testartist.bandcamp.com/album/testalbum",
+			expected: true,
+		},
+		{
+			desc:     "Valid track URL",
+			url:      "https://testartist.bandcamp.com/track/testtrack",
+			expected: true,
+		},
+		{
+			desc:     "Invalid URL - wrong path",
+			url:      "https://testartist.bandcamp.com/invalid/path",
+			expected: false,
+		},
+		{
+			desc:     "Invalid URL - missing path",
+			url:      "https://testartist.bandcamp.com",
+			expected: false,
+		},
+		{
+			desc:     "Invalid URL - wrong domain",
+			url:      "https://testartist.wrongdomain.com/music",
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		s.Run(tc.desc, func() {
+			u, err := url.Parse(tc.url)
+			s.Require().NoError(err)
+			result := isValidBandcampURL(u)
+			s.Equal(tc.expected, result)
+		})
+	}
+}
+
+func (s *HandlerTestSuite) Test_matchURLPattern() {
+	testCases := []struct {
+		desc     string
+		url      string
+		pattern  string
+		expected bool
+	}{
+		{
+			desc:     "Match discography URL",
+			url:      "https://testartist.bandcamp.com/music",
+			pattern:  "https://{artist}.bandcamp.com/music",
+			expected: true,
+		},
+		{
+			desc:     "Match album URL",
+			url:      "https://testartist.bandcamp.com/album/testalbum",
+			pattern:  "https://{artist}.bandcamp.com/album/{album}",
+			expected: true,
+		},
+		{
+			desc:     "Match track URL",
+			url:      "https://testartist.bandcamp.com/track/testtrack",
+			pattern:  "https://{artist}.bandcamp.com/track/{track}",
+			expected: true,
+		},
+		{
+			desc:     "No match - wrong path",
+			url:      "https://testartist.bandcamp.com/wrong/path",
+			pattern:  "https://{artist}.bandcamp.com/music",
+			expected: false,
+		},
+		{
+			desc:     "No match - extra path component",
+			url:      "https://testartist.bandcamp.com/album/testalbum/extra",
+			pattern:  "https://{artist}.bandcamp.com/album/{album}",
+			expected: false,
+		},
+		{
+			desc:     "No match - wrong domain",
+			url:      "https://testartist.wrongdomain.com/music",
+			pattern:  "https://{artist}.bandcamp.com/music",
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		s.Run(tc.desc, func() {
+			result := matchURLPattern(tc.url, tc.pattern)
+			s.Equal(tc.expected, result)
+		})
+	}
+}
+
+func (s *HandlerTestSuite) Test_Scrapp() {
+	testCases := []struct {
+		desc           string
+		url            string
+		scrapper       *scrapper.MockScrapper
+		expectedStatus int
+		expectedBody   string
+	}{
+		{
+			desc:           "Valid discography URL",
+			url:            "https://testartist.bandcamp.com/music",
+			scrapper:       s.mockDiscographyScrapper,
+			expectedStatus: http.StatusOK,
+			expectedBody:   "Request processed successfully",
+		},
+		{
+			desc:           "Valid album URL",
+			url:            "https://testartist.bandcamp.com/album/testalbum",
+			scrapper:       s.mockAlbumScrapper,
+			expectedStatus: http.StatusOK,
+			expectedBody:   "Request processed successfully",
+		},
+		{
+			desc:           "Valid track URL",
+			url:            "https://testartist.bandcamp.com/track/testtrack",
+			scrapper:       s.mockTrackScrapper,
+			expectedStatus: http.StatusOK,
+			expectedBody:   "Request processed successfully",
+		},
+		{
+			desc:           "Invalid URL - wrong domain",
+			url:            "https://testartist.wrongdomain.com/music",
+			expectedStatus: http.StatusBadRequest,
+			expectedBody:   "Invalid Bandcamp URL",
+		},
+		{
+			desc:           "Invalid URL - wrong path",
+			url:            "https://testartist.bandcamp.com/wrong/path",
+			expectedStatus: http.StatusBadRequest,
+			expectedBody:   "Invalid Bandcamp URL",
+		},
+		{
+			desc:           "Empty URL",
+			url:            "",
+			expectedStatus: http.StatusBadRequest,
+			expectedBody:   "Scrapp url param is required",
+		},
+	}
+
+	for _, tc := range testCases {
+		req, err := http.NewRequest("GET", "/api/v1/scrapp", nil)
+		s.Require().NoError(err)
+		q := req.URL.Query()
+		q.Add("url", tc.url)
+		req.URL.RawQuery = q.Encode()
+
+		if tc.scrapper != nil {
+			tc.scrapper.EXPECT().Execute(gomock.Any()).Return(nil).AnyTimes()
+		}
+
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(s.handler.Scrapp)
+
+		handler.ServeHTTP(rr, req)
+
+		s.Equal(tc.expectedStatus, rr.Code)
+		s.Equal(tc.expectedBody, strings.TrimSpace(rr.Body.String()))
+	}
 }
