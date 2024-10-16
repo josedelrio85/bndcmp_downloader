8175caf9512fbbd739b0bbd3fa237e7876cc6ef7
Jose del Río <josedelrio85@gmail.com>
2024-10-16 11:29:40 +0200

    added localhost with port to cors exlcusion

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -50,7 +50,7 @@ func setupRouter(httpHandler *handler.HttpHandler) http.Handler {
 	apiV1.HandleFunc("/scrapp", httpHandler.Scrapp).Methods("GET")
 
 	c := cors.New(cors.Options{
-		AllowedOrigins: []string{"http://localhost:5173"}, // Add your frontend URL here
+		AllowedOrigins: []string{"http://localhost:5173", "http://localhost:8080"}, // Add your frontend URL here
 		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
 		AllowedHeaders: []string{"*"},
 	})
